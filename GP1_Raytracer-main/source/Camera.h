c363156dae00c215b7e6887ef95cdecce26bb5e5
Vik Praet <vik.praet@gmail.com>
2023-09-28 18:27:46 +0200

    Ray-Tracer up to hard shadows

---
+++ GP1_Raytracer-main/source/Camera.h
@@ -0,0 +1,109 @@
+#pragma once
+#include <cassert>
+#include <SDL_keyboard.h>
+#include <SDL_mouse.h>
+
+#include "Math.h"
+#include "Timer.h"
+
+#include <iostream>
+
+namespace dae
+{
+	struct Camera
+	{
+		Camera() = default;
+
+		Camera(const Vector3& _origin, float _fovAngle):
+			origin{_origin},
+			fovAngle{_fovAngle}
+		{
+		}
+
+
+		Vector3 origin{};
+		float fovAngle{75.f};
+		float fovValue{tanf(fovAngle * (M_PI/180.f) / 2.f) };
+
+		Vector3 forward{ Vector3::UnitZ };
+		Vector3 up{Vector3::UnitY};
+		Vector3 right{Vector3::UnitX};
+
+		float totalPitch{0.f};
+		float totalYaw{0.f};
+
+		Matrix cameraToWorld{};
+
+
+		Matrix CalculateCameraToWorld()
+		{
+			right = Vector3::Cross(Vector3::UnitY, forward);
+			right.Normalize();
+			
+			up = Vector3::Cross(forward, right);
+			up.Normalize();
+			return { right, up, forward, origin };
+		}
+
+		void Update(Timer* pTimer)
+		{
+			const float deltaTime = pTimer->GetElapsed();
+
+			//Keyboard Input
+			const uint8_t* pKeyboardState = SDL_GetKeyboardState(nullptr);
+
+			const float SPEED{10};
+			if (pKeyboardState[SDL_SCANCODE_W])
+			{
+				origin.x += forward.x *  SPEED * deltaTime;
+				origin.z += forward.z *  SPEED * deltaTime;
+			}
+			if (pKeyboardState[SDL_SCANCODE_S])
+			{
+				origin.x -= forward.x * SPEED * deltaTime;
+				origin.z -= forward.z * SPEED * deltaTime;
+			}
+			if (pKeyboardState[SDL_SCANCODE_A])
+			{
+				origin.x -= right.x * SPEED * deltaTime;
+				origin.z -= right.z * SPEED * deltaTime;
+			}
+			if (pKeyboardState[SDL_SCANCODE_D])
+			{
+				origin.x += right.x * SPEED * deltaTime;
+				origin.z += right.z * SPEED * deltaTime;
+			}
+
+
+			const float FOV_INCREMENT	{ 20 };
+			const int MAX_FOV			{ 179 };
+			const int MIN_FOV			{ 10 };
+
+			if (pKeyboardState[SDL_SCANCODE_RIGHT] && fovAngle < MAX_FOV)
+			{
+				fovAngle += FOV_INCREMENT * deltaTime;
+				fovValue = tanf(fovAngle * (M_PI / 180.f) / 2.f);
+			}
+			if (pKeyboardState[SDL_SCANCODE_LEFT] && fovAngle > MIN_FOV)
+			{
+				fovAngle -= FOV_INCREMENT * deltaTime;
+				fovValue = tanf(fovAngle * (M_PI / 180.f) / 2.f);
+			}
+
+			//Mouse Input
+			int mouseX{}, mouseY{};
+			const uint32_t mouseState = SDL_GetRelativeMouseState(&mouseX, &mouseY);
+			
+			const float SENSITIVITY{ 0.007f };
+			totalPitch += mouseX * SENSITIVITY;
+			totalYaw -= mouseY * SENSITIVITY;
+			Matrix final{ Matrix::CreateRotationX(totalYaw) * Matrix::CreateRotationY(totalPitch) };
+
+			forward = final.TransformVector(Vector3::UnitZ);
+			forward.Normalize();
+
+			//todo: W2
+			//assert(false && "Not Implemented Yet");
+		}
+	};
+}
---
+++ GP1_Raytracer-main/source/Renderer.cpp
@@ -0,0 +1,82 @@
+//External includes
+#include "SDL.h"
+#include "SDL_surface.h"
+
+//Project includes
+#include "Renderer.h"
+#include "Math.h"
+#include "Matrix.h"
+#include "Material.h"
+#include "Scene.h"
+#include "Utils.h"
+
+using namespace dae;
+
+Renderer::Renderer(SDL_Window * pWindow) :
+	m_pWindow(pWindow),
+	m_pBuffer(SDL_GetWindowSurface(pWindow))
+{
+	//Initialize
+	SDL_GetWindowSize(pWindow, &m_Width, &m_Height);
+	m_pBufferPixels = static_cast<uint32_t*>(m_pBuffer->pixels);
+}
+
+void Renderer::Render(Scene* pScene) const
+{
+	Camera& camera = pScene->GetCamera();
+	auto& materials = pScene->GetMaterials();
+	auto& lights = pScene->GetLights();
+
+	float aspectRatio{ float(m_Width) / float(m_Height) };
+
+	for (int px{}; px < m_Width; ++px)
+	{
+		for (int py{}; py < m_Height; ++py)
+		{
+			float x = (2.f * ((static_cast<float>(px) + 0.5f) / static_cast<float>(m_Width)) - 1.f) * aspectRatio * camera.fovValue;
+			float y = (1.f - 2.f * ((static_cast<float>(py) + 0.5f) / static_cast<float>(m_Height))) * camera.fovValue;
+			Vector3 rayDirection{ x, y, 1 };
+
+			rayDirection.Normalize();
+			rayDirection = camera.CalculateCameraToWorld().TransformVector(rayDirection);
+			Ray hitray{ camera.origin, rayDirection };
+			
+			ColorRGB finalColor{};
+
+			HitRecord closestHit{};
+			pScene->GetClosestHit(hitray, closestHit);
+
+			if (closestHit.didHit)
+			{
+				finalColor = materials[closestHit.materialIndex]->Shade();
+				for (int i{}; i < pScene->GetLights().size(); ++i)
+				{
+					Vector3 LightVector{ LightUtils::GetDirectionToLight(pScene->GetLights()[i], closestHit.origin) };
+					Ray lightRayDirection{ closestHit.origin + closestHit.normal * 0.001f, LightVector.Normalized() };
+					lightRayDirection.max = LightVector.Magnitude();
+					if (pScene->DoesHit(lightRayDirection))
+					{
+						finalColor *= 0.5f;
+					}
+				}
+			}
+
+			//Update Color in Buffer
+			finalColor.MaxToOne();
+
+			m_pBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBuffer->format,
+				static_cast<uint8_t>(finalColor.r * 255),
+				static_cast<uint8_t>(finalColor.g * 255),
+				static_cast<uint8_t>(finalColor.b * 255));
+		}
+	}
+
+	//@END
+	//Update SDL Surface
+	SDL_UpdateWindowSurface(m_pWindow);
+}
+
+bool Renderer::SaveBufferToImage() const
+{
+	return SDL_SaveBMP(m_pBuffer, "RayTracing_Buffer.bmp");
+}
